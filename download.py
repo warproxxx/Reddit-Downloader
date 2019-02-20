8c052d5284fc28ff6eeb8a6a820aaae3b292ea76
Daniel Sapkota <daniel@quantorithm.com>
2019-02-20 12:18:31 +0545

    Windows compatibility

--- download.py
+++ download.py
@@ -1,4 +1,5 @@
 from redditManager import redditManager
 
-rm = redditManager()
-rm.downloader(["technology", "pics", "gaming", "todayilearned", "UnethicalLifeProTips", "KeepOurNetFree", "coolguides", "explainlikeimfive", "talesfromtechsupport", "frugal", "piracy", "worldnews", "pcmasterrace", "television", "news", "political_revolution", "canada", "the_donald", "australia", "apple", "android", "sysadmin", "geek", "steam", "programmerhumor", "nostupidquestions", "outoftheloop", "youshouldknow", "netsec", "lifeprotips", "india", "darkwebs", "VPN", "vpnreviews", "vpncoupons", "privacy", "netsec", "TOR"])
+if __name__ == '__main__':
+    rm = redditManager()
+    rm.downloader(["technology", "pics", "gaming", "todayilearned", "UnethicalLifeProTips", "KeepOurNetFree", "coolguides", "explainlikeimfive", "talesfromtechsupport", "frugal", "piracy", "worldnews", "pcmasterrace", "television", "news", "political_revolution", "canada", "the_donald", "australia", "apple", "android", "sysadmin", "geek", "steam", "programmerhumor", "nostupidquestions", "outoftheloop", "youshouldknow", "netsec", "lifeprotips", "india", "darkwebs", "VPN", "vpnreviews", "vpncoupons", "privacy", "netsec", "TOR"])
